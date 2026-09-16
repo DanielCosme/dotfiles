dc6c681ec0581a1dfb494bbc8486fe281e1b963f
Daniel Cosme <cosmedaniel8@gmail.com>
2026-09-16 16:29:54 -0400

    Update

--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -103,6 +103,7 @@ set -U fish_pager_color_progress brwhite --background=cyan
 set -U fish_color_match --background=brblue
 set -U fish_color_comment 990000
 
+
 # >>> grok installer >>>
 fish_add_path $HOME/.grok/bin
 # <<< grok installer <<<
